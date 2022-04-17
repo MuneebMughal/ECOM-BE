135f01025bd2d6ceb54714bef71c8a8e7dd753f1
Muneeb <muneebmughalr@gmail.com>
2022-04-17 22:56:49 +0500

    some basic changes

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { upload, remove } = require("../controllers/cloudinary");
-const { authCheck, adminCheck } = require("../middlewares/auth");
+// const { authCheck, adminCheck } = require("../middlewares/auth");
 router.post("/image-upload", upload);
 router.delete("/image-remove/:id", remove);
 module.exports = router;
